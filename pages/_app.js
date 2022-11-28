06d418c08e5a961dfa0bce592d5ec3cdb3179033
camouflagee1 <saurabhn420@gmail.com>
2022-11-28 05:32:22 +0530

    complete

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,6 @@ function MyApp({ Component, pageProps }) {
     <Head>
         <title>NextCharts - Data Visualisation</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,500,0,200&display=swap" />
     </Head>
     <ChakraProvider>
       <NavBar />
